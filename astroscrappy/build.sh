59bcf9664af91d53193bc5081f4908b79379c316
Josh Alexander <obviousrebel@users.noreply.github.com>
2019-02-17 16:03:19 -0500

    recipe updates to fix ah_bootstrap and setuptools (#463)

--- astroscrappy/build.sh
+++ astroscrappy/build.sh
@@ -1,1 +1,2 @@
+curl https://raw.githubusercontent.com/astropy/astropy-helpers/master/ah_bootstrap.py > ah_bootstrap.py
 $PYTHON setup.py install --offline --no-git --single-version-externally-managed --record record.txt
--- ccdproc/build.sh
+++ ccdproc/build.sh
@@ -1,1 +1,2 @@
+curl https://raw.githubusercontent.com/astropy/astropy-helpers/master/ah_bootstrap.py > ah_bootstrap.py
 $PYTHON setup.py install
