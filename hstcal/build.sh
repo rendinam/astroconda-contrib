a4ad76f33c2493ac5a50bac604587a47be763dae
Matt Rendina <rendinam@users.noreply.github.com>
2020-01-17 16:56:20 -0500

    Update recipe to use operational toolchains (#596)
    Conda-supplied compilers used for linux.
    OS-provided compilers are used for Macos.

--- hstcal/build.sh
+++ hstcal/build.sh
@@ -1,5 +1,12 @@
+# Use the compilers provided by conda. The activation of the build
+# environment defines the GCC and GFORTRAN env vars.
+export CC=$CC
+
+export FCFLAGS="$FCFLAGS -fPIC"
+
 if [[ `uname` == Darwin ]]; then
-    export CC=`which gcc`
+    export CC=$(which $CLANG)
+    export GFORTRAN=$(which gfortran)
     if [[ `uname -m` == x86_64 ]]; then
         export CFLAGS="$CFLAGS -m64"
         export LDFLAGS="$LDFLAGS -m64"
