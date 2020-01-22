76fad0a9b42d4978b40d96fc791ce26bb1c165f8
Matt Rendina <rendinam@users.noreply.github.com>
2020-01-22 11:46:51 -0500

    Adjust to correctly find compiler on macos (#597)

--- hstcal/build.sh
+++ hstcal/build.sh
@@ -1,16 +1,16 @@
-# Use the compilers provided by conda. The activation of the build
-# environment defines the GCC and GFORTRAN env vars.
-export CC=$CC
 
 export FCFLAGS="$FCFLAGS -fPIC"
 
 if [[ `uname` == Darwin ]]; then
-    export CC=$(which $CLANG)
-    export GFORTRAN=$(which gfortran)
+    #export GFORTRAN=$(which gfortran)
     if [[ `uname -m` == x86_64 ]]; then
         export CFLAGS="$CFLAGS -m64"
         export LDFLAGS="$LDFLAGS -m64"
     fi
+else
+    # Use the compilers provided by conda. The activation of the build
+    # environment defines the GCC and GFORTRAN env vars.
+    export CC=$CC
 fi
 
 ./waf configure --prefix=$PREFIX --release-with-symbols
