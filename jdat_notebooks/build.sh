3b2cdc79999623ebad6a18b6df19091048d3adbf
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2020-03-31 11:31:44 -0400

    Initial commit of jdat_notebooks (#633)
    * Initial commit of jdat_notebooks
    
    * Added initial semantic version
    
    * Minor improvements:
    
    * Increase verbosity
    * Exclude .git* related files in destination
    * Include environment.yml in destination
    
    Co-authored-by: Joseph Curtin <jbcurtincode@gmail.com>

---
+++ jdat_notebooks/build.sh
@@ -0,0 +1,14 @@
+if [[ -z ${PKG_NAME} ]]; then
+    echo "Conda build has changed out from under you. PKG_NAME is undefined..."
+    exit 1
+fi
+
+dest="${PREFIX}/share/${PKG_NAME}"
+usedir="$(pwd)"
+
+if [[ -d ${usedir}/work ]]; then
+    usedir=${usedir}/work
+fi
+
+mkdir -p "${dest}"
+rsync -av --exclude '.git*' "${usedir}"/environment.yml "${usedir}"/notebooks/ "${dest}"/
